5a27a1974884af6e12e5c6859227e9a31dfb164f
AndreD4 <andremai629@gmail.com>
2023-09-26 17:00:30 +0900

    Examples of I/O

--- BasicIO/main.cpp
+++ BasicIO/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main() 
 {
-  cout << "Hello World" << endl; 
+  
+// cout << "Hello World" << endl; 
     
 //  cout << "Hello";
 //  cout << "World" << endl;
@@ -18,7 +19,7 @@ int main()
 //    int num1;
 //    int num2;
 //    double num3;
-
+//
 //    cout <<  "Enter an integer: ";
 //    cin >> num1;
 //    cout << "You entered: " << num1 << endl;
