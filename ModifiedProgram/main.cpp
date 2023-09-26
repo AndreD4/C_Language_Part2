cd81cc8b018a34783beada88cd7fe2214be5ed66
AndreD4 <andremai629@gmail.com>
2023-09-26 15:11:22 +0900

    Modified Program

---
+++ ModifiedProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int favourite_Number;
+    std::cout << "Enter your favourite numebr between 1-100:";
+    std::cin >> favourite_Number;
+    std::cout << "Amazing! thats my favourite number too!" << std::endl;
+    std::cout << "No really!! " << favourite_Number <<" is my favourite number too!" << std::endl;
+    return 0;
+}
